fd1b68e02aacc336fbf91f12af3e66e37600a64e
RONIX-HUB <hubronix@gmail.com>
2025-09-03 21:50:21 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.getElementById("playButton").addEventListener("click", function() {
+    const song = document.getElementById("mySong");
+    if (song.paused) {
+        song.play();
+    } else {
+        song.pause();
+    }
+});
