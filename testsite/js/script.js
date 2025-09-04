4764794492bd09d06c8108c8552cf05b23e1ea3c
RONIX-HUB <hubronix@gmail.com>
2025-09-03 21:49:56 -0700

    Delete testsite directory

--- testsite/js/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("playButton").addEventListener("click", function() {
-    const song = document.getElementById("mySong");
-    if (song.paused) {
-        song.play();
-    } else {
-        song.pause();
-    }
-});
